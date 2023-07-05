8d65c1b172d6aa78974d3f84971f59aa0ed710a0
Ritik <ritikrajbhagat1@gmail.com>
2023-07-05 23:57:59 +0530

    staters added using bootstrap and items layer created

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Staters from '../helpers.js/Staters'
+import Items from '../helpers.js/Items'
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
-      body
+      {/* carousel */}
+      <Staters/>
+      <Items/>
       <Footer/>
     </div>
   )
