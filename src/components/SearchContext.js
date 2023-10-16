7cc44d3f7b310fb24746cc7a1befd18e9255f97e
Ritik <ritikrajbhagat1@gmail.com>
2023-10-16 20:17:26 +0530

    silly mistake of searching taken care of

--- src/components/SearchContext.js
+++ src/components/SearchContext.js
@@ -29,7 +29,7 @@ export const SearchProvider = ({children}) => {
             }
       } ;
       useEffect(() => {
-            if(query !== ""){
+            if(id !== ""){
                   FetchSearchedData() ;
             }
       }, [query])
